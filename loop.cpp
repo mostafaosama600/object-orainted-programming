0b0391a65a03880cc25ffdb9d7742816fcbb3891
mostafa osama <mostafaelshrombably775@gmail.com>
2023-01-28 20:50:34 +0200

    while

--- loop.cpp
+++ loop.cpp
@@ -1,70 +1,66 @@
 #include <iostream>
 using namespace std;
 
-// int main()
-// {
-//     int count = 0;
-//     for (int i = 1; i <= 10; i++)
-//     {
-//         if (i % 2 == 0 || i % 3 == 0)
-//         {
-//             count++;
-//         }
-//     }
-//     cout << count << endl;
-// }
-// int main()
-// {
-//     int sum = 0;
-//     for (int i = 1; i <= 10; i++)
-//     {
-//         sum += i;
-//     }
-//     cout << "sum =" << sum << endl;
-// }
-// int main()
-// {
-//     int x;
-//     cin >> x;
-//     int fact = 1;
-//     for (int i = 1; i <= x; i++)
-//     {
-//         fact *= i;
-//     }
-//     cout << "fact =" << fact << endl;
-// }
-// int main()
-// {
-//     int x;
-//     cin >> x;
-//     int count = 0;
-//     for (int i = 1; i <= x; i++)
-//     {
-//         if (x % i == 0)
-//         {
-//             count++;
-//         }
-//     }
-//     if (count == 2)
-//     {
-//         cout << x << " is a prime number";
-//     }
-//     else
-//     {
-//         cout << x << " is not a prime number";
-//     }
-// }
-
 int main()
 {
-    // Multiplication table
-    for (int i = 1; i <= 12; i++)
+    //     int x;
+    //     cin >> x;
+    //     int count = 0;
+    //     for (int i = 1; i <= x; i++)
+    //     {
+    //         if (x % i == 0)
+    //         {
+    //             count++;
+    //         }
+    //     }
+    //     if (count == 2)
+    //     {
+    //         cout << x << " is a prime number";
+    //     }
+    //     else
+    //     {
+    //         cout << x << " is not a prime number";
+    //     }
+
+    //     // Multiplication table
+    //     for (int i = 1; i <= 12; i++)
+    //     {
+    //         for (int j = 1; j <= 12; j++)
+    //         {
+    //             cout << j << " * " << i << " = " << j * i << endl;
+    //         }
+    //         cout << "\n--------------------\n"
+    //              << endl;
+    //     }
+
+    // squar
+    // int x;
+    // cin >> x;
+    // for (int i = 1; i <= x; i++)
+    // {
+    //     for (int j = 1; j <= x; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+    // int x;
+    // cin >> x;
+    // int i = 1;
+    // while (i <= x)
+    // {
+    //     if (i % 2 != 0)
+    //     {
+    //         cout << i << endl;
+    //     }
+    //     i++;
+    // }
+    for (int i = 1; i <= 10; i++)
     {
-        for (int j = 1; j <= 12; j++)
+        if (i == 4)
         {
-            cout << j << " * " << i << " = " << j * i << endl;
+            break;
         }
-        cout << "\n--------------------\n"
-             << endl;
+        cout << i << endl;
     }
 }
