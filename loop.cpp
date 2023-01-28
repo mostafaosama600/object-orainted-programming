6573bc881e1e0ca7fe09d0ed8b5021fa61452e40
mostafa osama <mostafaelshrombably775@gmail.com>
2023-01-28 19:10:29 +0200

    factorial

--- loop.cpp
+++ loop.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 using namespace std;
 
+// int main()
+// {
+//     int count = 0;
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         if (i % 2 == 0 || i % 3 == 0)
+//         {
+//             count++;
+//         }
+//     }
+//     cout << count << endl;
+// }
+// int main()
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         sum += i;
+//     }
+//     cout << "sum =" << sum << endl;
+// }
 int main()
 {
-    int count = 0;
-    for (int i = 1; i <= 10; i++)
+    int x;
+    cin >> x;
+    int fact = 1;
+    for (int i = 1; i <= x; i++)
     {
-        if (i % 2 == 0 || i % 3 == 0)
-        {
-            count++;
-        }
+        fact *= i;
     }
-    cout << count << endl;
+    cout << "fact =" << fact << endl;
 }
