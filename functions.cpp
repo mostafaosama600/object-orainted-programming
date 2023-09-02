73c32e2e16e780c3de31607eb8e497faed327536
mostafa osama <mostafaelshrombably775@gmail.com>
2023-09-02 17:06:52 +0300

    examples

--- functions.cpp
+++ functions.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+int calculation(int x, int y)
+{
+    return x + y;
+}
+int sayHello()
+{
+    cout << "Hello";
+}
+
+int getMaxValue(int x, int y)
+{
+    if (x > y)
+    {
+        return x;
+    }
+    else
+    {
+        return y;
+    }
+}
+
+void printName()
+{
+    cout << "Hello Mostafa";
+}
 
 int main()
 {
+    calculation(5, 5);
+    sayHello();
+
+    cout << "max value is :";
+    getMaxValue(10, 15);
+    printName();
 }
