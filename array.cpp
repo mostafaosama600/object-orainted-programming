77c221062c4209c280ad6199238823777b9f779d
mostafa osama <mostafaelshrombably775@gmail.com>
2023-09-03 15:51:19 +0300

    array

--- array.cpp
+++ array.cpp
@@ -4,18 +4,36 @@ using namespace std;
 int main()
 {
 
-    int arr[10];
-    int max = 0;
-    for (int i = 0; i < 10; i++)
-    {
-        cin >> arr[i];
-        if (arr[i] > max)
-        {
-            max = arr[i];
-        }
-        else
-        {
-            cout << "max =" << max << endl;
-        }
-    }
+    // int arr[10];
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     cin >> arr[i];
+    //     if(arr[i] / 2){
+    //         cout << arr[i] << "is a prime number" << endl;
+    //     }else{
+    //         cout << arr[i] << "not a prime number" << endl;
+    //     }
+    // }
+
+    // ================================= * =================================
+
+    // int x;
+    // cin >> x;
+    // int count = 0;
+
+    // for (int j = 0; j <= x; j++)
+    // {
+    //     if (x % j == 0)
+    //     {
+    //         count++;
+    //     }
+    // }
+    // if (count == 2)
+    // {
+    //     cout << x << "prime number\n";
+    // }
+    // else
+    // {
+    //     cout << x << "not prime number\n";
+    // }
 }
