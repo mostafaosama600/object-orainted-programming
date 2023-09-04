4c68844233a0fb178a5825a5258ef8660c43cfbd
mostafa osama <mostafaelshrombably775@gmail.com>
2023-09-04 15:01:22 +0300

    revision

--- array.cpp
+++ array.cpp
@@ -3,21 +3,4 @@ using namespace std;
 
 int main()
 {
-    // Multi Dimantion Array
-    int arr[4][3];
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            cin >> arr[i][j];
-        }
-    }
-    cout << "\n----------------\n";
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            cout << arr[i][j];
-        }
-    }
 }
